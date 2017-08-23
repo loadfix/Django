d87f9538ca45054431b28021f6f69151dd47c210
Ben Hooper <ben@ayrton.com.au>
2017-08-23 21:40:11 +0800

    Initial Commit

--- companies/scripts/csv2post.py
+++ companies/scripts/csv2post.py
@@ -8,6 +8,8 @@ import datetime
 from requests.utils import requote_uri
 import traceback
 from bs4 import BeautifulSoup
+import signal
+
 
 companies_url = "http://127.0.0.1:8000/companies/api/companies/"
 directors_url = "http://127.0.0.1:8000/companies/api/directors/"
@@ -18,57 +20,66 @@ nasdaq_url = "http://www.nasdaq.com/screening/companies-by-industry.aspx?exchang
 nyse_url = "http://www.nasdaq.com/screening/companies-by-industry.aspx?exchange=NYSE&render=download"
 amex_url = "http://www.nasdaq.com/screening/companies-by-industry.aspx?exchange=AMEX&render=download"
 asx_url = "http://www.asx.com.au/asx/research/ASXListedCompanies.csv"
+hkse_url = "https://www.hkex.com.hk/eng/market/sec_tradinfo/stockcode/eisdeqty.htm"
+
 
 nasdaq_csv_file = 'nasdaq.csv'
 amex_csv_file = 'amex.csv'
 nyse_csv_file = 'nyse.csv'
 asx_csv_file = 'asx.csv'
+hkse_csv_file = 'hkse.csv'
 
 error_messages = []
-
+debug = True
 
 independent_director_titles = [
-    "Independent Director",
-    "Member of the Supervisory Board",
-    "Independent Trustee",
-    "Non-Executive Member of the Board of Directors",
-    "Non-Executive Independent Director",
     "Independent Chairman of the Board",
     "Independent Chairman of the Board of Trustees",
-    "Non-Executive Director",
-    "Non-Executive Chairman of the Board",
-    "Non-Executive Independent Director of Responsible Entity",
+    "Independent Director",
     "Independent Non-Executive Chairman of the Board of Responsible Entity",
-    "Independent Non-Executive Director",
     "Independent Non-Executive Chairperson of the Board",
     "Independent Non-Executive Chairman of the Board",
-    "Non-Executive Independent Chairman of the Board",
+    "Independent Non-Executive Director",
+    "Independent Non-Executive Director, Company Secretary",
+    "Independent Trustee",
+    "Non-Executive Director",
     "Non-Executive Chairman",
+    "Non-Executive Chairman of the Board",
+    "Non-Executive Deputy Chairman of the Board",
+    "Non-Executive Independent Chairman of the Board",
+    "Non-Executive Independent Director",
+    "Non-Executive Independent Director of Responsible Entity",
+    "Non-Executive Member of the Board of Directors",
+    "Member of the Supervisory Board",
+
     "Lead Independent Non-Executive Director",
     "IndependentChairman of the Board",
-    "Chairman of the Board, Chief Executive Officer"
+    "Company Secretary, Non-Executive Director, Investor Relations Contact Officer"
 ]
 
 director_titles = [
-    "Director",
     "Chairman",
     "Chairman of the Board",
-    "Executive Chairman of the Board, Company Secretary",
-    "Deputy Chairman of the Board",
-    "Member of the Board of Directors",
-    "Chairman of the Supervisory Board",
-    "Member of the Supervisory Board",
-    "Member of the Management Board",
+    "Chairman of the Board, Chief Executive Officer",
     "Chairman of the Executive Board",
-    "Trustee",
-    "Executive Director",
-    "Executive Chairman of the Board, Founder",
-    "Non-Independent Non-Executive Director",
-    "Executive Non-independent Director",
+    "Deputy Chairman of the Board",
+    "Director",
+    "Director of Responsible Entity",
     "Executive Chairman of the Board",
     "Executive Chairman of the Board, Chief Executive Officer",
+    "Executive Chairman of the Board, Company Secretary",
+    "Chairman of the Supervisory Board",
+    "Executive Chairman of the Board, Founder",
+    "Executive Director",
+    "Executive Director, Company Secretary",
+    "Executive Non-independent Director",
+    "Member of the Board of Directors",
+    "Member of the Supervisory Board",
+    "Member of the Management Board",
+    "Trustee",
+    "Non-Independent Non-Executive Director",
     "Acting Executive Chairman of the Board, Chief Executive Officer, Managing Director",
-    "Executive Director, Company Secretary"
+    "Finance Director, Managing Director, Executive Director, Company Secretary"
 ]
 director_titles.extend(independent_director_titles)
 
@@ -86,6 +97,22 @@ director_suffix = [
 director_suffix.extend(independent_director_suffix)
 
 
+mens_names = [
+    "James", "Michael", "Peter", "Silvio", "David",  "Robert", "Grant", "John", "Geoffrey", "Ross", "Paul", "Wayne",
+    "Keith", "Christopher", "Andrew", "Adam", "Matt", "Brett", "Stuart", "Anthony", "Craig", "Frank", "Vincent",
+    "Marcus", "Hamish", "Stephen", "Philip", "Bryce", "Ivan", "Alvin", "Alan", "Simon", "Geoff", "Neil", "Nathan",
+    "Jeremy", "Charles", "Gregory", "Alasdair", "Ben", "Jeremy", "Peter", "Brian", "Brad", "Graeme", "Colin", "Harald",
+    "Daniel", "Leonard", "Jonathan", "Barry", "Scott", "Dennis", "Rodney", "Thomas", "Darren", "Nicholas", "Raymond",
+    "Malcolm", "Gavin",
+
+]
+womens_names = [
+    "Natalya", "Rosemary", "Rebekah", "Elizabeth", "Jacqueline", "Belinda", "Diane", "Heather", "Linda", "Anna", "Liza",
+    "Karen", "Nancy", "Phillipa", "Rebecca",
+]
+
+
+
 header = {
     'Content-Type': 'application/json'
     }
@@ -93,6 +120,17 @@ header = {
 company_file = "companies.csv"
 director_file = "directors.csv"
 
+
+# Catch Control-C
+start_time = datetime.datetime.now()
+def signal_handler(signal, frame):
+   end_time = datetime.now()
+   print(str(end_time - start_time) + " seconds elapsed")
+   sys.exit(0)
+
+
+# Catch Control-C, flush buffer to CSV
+signal.signal(signal.SIGINT, signal_handler)
 
 class Company:
 
@@ -112,11 +150,11 @@ class Director:
     def __init__(self):
         director_id = -1
         name = ""
-        age = ""
+        age = -1
         sex = ""
         birth_date = ""
         title = ""
-        tenure = ""
+        start_date = ""
         is_independent = False
 
 
@@ -134,6 +172,20 @@ def get_csv_files():
     urllib.request.urlretrieve(nasdaq_url, nasdaq_csv_file)
     urllib.request.urlretrieve(amex_url, amex_csv_file)
 
+    # Do something special for HKSE as it is a web page
+    with open(hkse_csv_file, 'w') as csvfile:
+        fieldnames = ['symbol', 'name']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        http = urllib3.PoolManager()
+        r = http.request('GET', hkse_url)
+        soup = BeautifulSoup(r.data, "html.parser")
+        stock_table = soup.find_all('table')[0]('td')[0]('tr')[0]('tr')[3]('tr')
+        for x in range(1, len(stock_table)):
+            symbol = stock_table[x]('td')[0].text.strip()
+            stock = stock_table[x]('td')[1].text.strip()
+            writer.writerow({'symbol': symbol, 'name': stock})
 
 def ConvertMC(market_cap):
 
@@ -270,8 +322,9 @@ def add_directors():
             director_independent = True
 
         if director[5].strip() == "":
-            print("No age, setting to -1")
-            director_age = "-1"
+            if debug:
+                print("No age found, blanking")
+            director_age = ""
         else:
             director_age = str(director[5].strip())
 
@@ -319,8 +372,7 @@ def add_directors():
         try:
             # Get Company ID
             url = requote_uri(companies_url + "?format=json&name=" + director_company)
-            r = http.request('GET', url)
-            jsondata = json.loads(r.data.decode('utf-8'))
+            jsondata = getJSONData(url)
 
             # Found the company
             if len(jsondata) != 0:
@@ -331,8 +383,7 @@ def add_directors():
                 continue
 
             url = requote_uri(boards_url + "?format=json&company=" + company_id + "&director=" + director_id)
-            r = http.request('GET', url)
-            jsondata = json.loads(r.data.decode('utf-8'))
+            jsondata = getJSONData(url)
 
             if len(jsondata) == 0:
                 # Director is not on the board
@@ -356,6 +407,9 @@ def add_directors():
             print(str(e))
 
 
+###############
+# New Section #
+# #############
 def getCompanyFromReuters(symbol, exchange):
 
     if exchange == "AMEX":
@@ -365,7 +419,7 @@ def getCompanyFromReuters(symbol, exchange):
     if exchange == "NYSE":
         suffix = ".N"
     if exchange == "NASDAQ":
-        suffix = ".0"
+        suffix = ".O"
 
     reuters_url = "http://www.reuters.com/finance/stocks/overview?symbol="
     url = requote_uri(reuters_url + symbol + suffix)
@@ -374,7 +428,12 @@ def getCompanyFromReuters(symbol, exchange):
     r = http.request('GET', url)
     soup = BeautifulSoup(r.data, 'html.parser')
 
-    name = soup.find_all('h1')[0].text.split('(')[0]
+    try:
+        name = soup.find_all('h1')[0].text.split('(')[0]
+    except Exception as e:
+        print(str(e))
+        print("Could not find company " + symbol + " at Reuters")
+        return None
 
     try:
         market_cap = soup.find('tbody').find_all('td')[3].text
@@ -467,15 +526,25 @@ def add_ticker(ticker, exchange, company):
             'company': company.company_id
         }
         r = postJSONData(ticker_url, payload)
+        jsondata = json.loads(r.data.decode('utf-8'))
 
-        print(r.data)
+        print(jsondata)
 
         print("Added ticker " + ticker + " to company " + company.name)
     else:
-        print("Ticker " + ticker + " already associate to company " + str(company.company_id))
+        print("Ticker " + ticker + " already associated to company " + str(company.company_id))
 
 
 def getDirectorSexFromName(name):
+
+    for male_name in mens_names:
+        if name.startswith(male_name + " "):
+            return "M"
+
+    for female_name in womens_names:
+        if name.startswith(female_name + " "):
+            return "F"
+
     return "U"
 
 def isDirector(title):
@@ -513,7 +582,7 @@ def getDiretorsFromReuters(ticker, exchange):
     if exchange == "NYSE":
         suffix = ".N"
     if exchange == "NASDAQ":
-        suffix = ".0"
+        suffix = ".O"
 
     reuters_url = "http://www.reuters.com/finance/stocks/companyOfficers?symbol="
     url = requote_uri(reuters_url + ticker + suffix)
@@ -553,16 +622,20 @@ def getDiretorsFromReuters(ticker, exchange):
             this_director.title = title
 
             # Get name, age and sex of director
-            this_director.name = officer_table.find_all('tr')[y].find_all('td')[0].text.strip()
+            this_director.name = str(officer_table.find_all('tr')[y].find_all('td')[0].text.strip()).replace('\xa0',' ')
             this_director.age = officer_table.find_all('tr')[y].find_all('td')[1].text.strip()
+            if this_director.age == "":
+                this_director.age = ""
             this_director.sex = getDirectorSexFromName(this_director.name)
 
-            # Get tenure
+            # Get start date
             if officer_table.find_all('tr')[y].find_all('td')[2].text.strip() != "":
-                this_director.tenure = officer_table.find_all('tr')[y].find_all('td')[2].text.strip()
+                this_director.start_date = officer_table.find_all('tr')[y].find_all('td')[2].text.strip() + "-01-01"
+            else:
+                this_director.start_date = ""
 
             # Is this director independent?
-                this_director.is_independent = True
+            this_director.is_independent = isIndependentDirector(title)
 
             director_array.append(this_director)
         else:
@@ -570,6 +643,34 @@ def getDiretorsFromReuters(ticker, exchange):
 
 
     return director_array
+
+
+def getDirectorIDByName(director):
+
+    print("DEBUG:")
+    print("Age:" + str(director.age))
+
+    if str(director.age) == "":
+        url = requote_uri(directors_url + "?name=" + director.name)
+    else:
+        url = requote_uri(directors_url + "?name=" + director.name + "&age=" + str(director.age))
+
+
+    jsondata = getJSONData(url)
+
+    if len(jsondata) == 0:
+        # Director not found
+        director.director_id = -1
+
+    elif len(jsondata) == 1:
+        director.director_id = jsondata[0]['id']
+
+    else:
+        # More than one director was returned!!
+        # Return the number of duplicates as a negative director_id
+        director.director_id = 0 - len(jsondata)
+
+    return director
 
 def add_directors(company, ticker, exchange):
     if company.company_id is -1:
@@ -601,12 +702,74 @@ def add_directors(company, ticker, exchange):
         for director in director_array:
             print("Name: " + director.name)
             print("Age: " + director.age)
+
+            # See if director already exists
+            director = getDirectorIDByName(director)
+
+            if director.director_id == -1:
+                # Director not found, adding
+
+                if str(director.age) == "":
+                    payload = {
+                        'name': director.name,
+                        'sex': director.sex
+                    }
+                else:
+                    payload = {
+                        'name': director.name,
+                        'age': director.age,
+                        'sex': director.sex
+                    }
+
+                # Todo: Check return status
+                r = postJSONData(directors_url, payload)
+                jsondata = json.loads(r.data.decode('utf-8'))
+
+                if debug:
+                    print("Post Response:")
+                    print(jsondata)
+
+                director.director_id = str(jsondata['id'])
+
+                # Check if object was created
+                if debug:
+                    print("created new director with ID:" + director.director_id + " for company ID: " + company.company_id)
+
+                # Add director to board
+                if str(director.start_date) == "":
+                    payload = {
+                        'company': company.company_id,
+                        'director': director.director_id,
+                        'title': director.title,
+                        'is_independent': director.is_independent
+                    }
+                else:
+                    payload = {
+                        'company': company.company_id,
+                        'director': director.director_id,
+                        'title': director.title,
+                        'start_date': director.start_date,
+                        'is_independent': director.is_independent
+                    }
+
+                if debug:
+                    print("Sending payload:")
+                    print(payload)
+
+                r = postJSONData(boards_url, payload)
+                jsondata = json.loads(r.data.decode('utf-8'))
+                print(jsondata)
+                print("Added " + director.name + " to board of " + company.name )
+
+            else:
+                print("One or more directors exist with Name: " + director.name + " and Age: " + director.age)
     else:
         print("No directors found at " + company.name)
 
-    print("Non Director roles")
-    for position in set(error_messages):
-        print("===> " + position)
+    if len(error_messages) is not 0:
+        print("Non Director roles")
+        for position in set(error_messages):
+            print("===> " + position)
 
 def process_company(ticker, exchange):
     # Get the correct name and market cap from Reuters
@@ -614,6 +777,9 @@ def process_company(ticker, exchange):
 
     # Check if company already exists in the DB and set company ID
     this_company = getCompanyIDByName(this_company)
+
+    if this_company is None:
+        return
 
     if this_company.company_id is -1:
         # Company is not in database, add it
@@ -660,11 +826,14 @@ def process_csv(csv_file, exchange):
         process_company(company[ticker_column].strip(), exchange)
 
 
-
-
-
+###################################
+#             Runtime             #
+###################################
 #add_companies()
 #add_directors()
 #get_csv_files()
 
-process_csv(asx_csv_file,"ASX")
+#process_csv(asx_csv_file,"ASX")
+#process_csv(amex_csv_file,"AMEX")
+process_csv(nyse_csv_file,"NYSE")
+#process_csv(nasdaq_csv_file,"NASDAQ")
