e8ab51f5fc9007fc85a4f8dc6a8a41e1659a7568
Ben Hooper <ben@ayrton.com.au>
2018-07-17 02:53:55 +0800

    Move SECRET_KEY from settings.py to os.environ[]

--- Django/settings.py
+++ Django/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e4eh!itqqq-qo3d&9h)zj=1z#n_x033wnam&a#4$g@*3)(v*a7'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
